ba1181b192e7fa5ba907a7c4e886d1de42d9cdc3
Chris Tetreault <chris@doingmyprogramming.com>
2015-09-02 08:48:24 -0700

    Brought formatting up to my new standards
    appeased hlint

--- DmpHelper/Main.hs
+++ DmpHelper/Main.hs
@@ -7,20 +7,27 @@ import Data.List
 
 -- | Main. Parses arguments then calls execDmpHelper
 main :: IO ()
-main = do argRaw <- getArgs
-          argV <- return $ parseArgV argRaw
-          case argV of Left a -> printUsage
-                       Right b -> execDmpHelper b
+main = do
+   argRaw <- getArgs
+   let argV = parseArgV argRaw
+   case argV of
+      Left a -> printUsage
+      Right b -> execDmpHelper b
 
--- | Executes the parser                    
-execDmpHelper :: ArgV -> IO ()
-execDmpHelper argv = do inFile <- readFile $ inputFile argv
-                        printIfV argv inFile
-                        result <- return $ parseDmpMarkup inFile
-                        case result of Left a -> printIfV argv $ show a
-                                       Right b -> do printIfV argv b
-                                                     writeFile (outputFile argv) b
+-- | Executes the parser
+execDmpHelper :: ArgV
+                 -> IO ()
+execDmpHelper argv = do
+   inFile <- readFile $ inputFile argv
+   printIfV argv inFile
+   let result = parseDmpMarkup inFile
+   case result of Left a -> printIfV argv $ show a
+                  Right b -> do
+                     printIfV argv b
+                     writeFile (outputFile argv) b
 
--- | prints if --v                     
-printIfV :: ArgV -> String -> IO ()
-printIfV av s = if (isVerbose av) then putStrLn s else return ()
+-- | prints if --v
+printIfV :: ArgV
+            -> String
+            -> IO ()
+printIfV av s = when (isVerbose av) (putStrLn s)
